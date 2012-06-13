a1d8e70e1e7929ae1cd765969f5e9f8203a04194
none <alexander.podkidyshev@gmail.com>
2012-06-13 16:29:28 +0300

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsElfinderCkeditor::Application.config.secret_token = '6b88ff19e027d51eee9219ba766781087ba3abc98d44f006c85ed363d942f200d57d9c7082eff096089d7353385617532ebea7c147eb366df6299f4d9ca73acb'
